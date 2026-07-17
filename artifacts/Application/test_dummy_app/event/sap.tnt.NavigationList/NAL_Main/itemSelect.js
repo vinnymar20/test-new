5842ae767e695be786136df957b19b0660ac0111
vinnymar20 <vinny_marande@ifabula.com>
2026-07-17 14:47:54 +0700

    Delete artifacts/Application/test_dummy_app directory

--- artifacts/Application/test_dummy_app/event/sap.tnt.NavigationList/NAL_Main/itemSelect.js
+++
@@ -1,9 +0,0 @@
-// The following parameters are available via oEvent.getParameter("parameterName"); 
-// 
-// item - sap.ui.core.Item
-// 
-
-var menuKey =
-    oEvent.getParameter("item").getKey();
-
-navigateTo(menuKey);
